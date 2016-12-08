aaf84c7daf3186bf1da8593eec072a60d7d806af
imreyes <guangyang11231987@gmail.com>
2016-12-08 17:57:12 -0500

    Update rda file with hover

--- DevelopingDataProduct/Week3_ProgrammingAssignment/GasPollutionMetal_DataProcessor.R
+++ DevelopingDataProduct/Week3_ProgrammingAssignment/GasPollutionMetal_DataProcessor.R
@@ -1,3 +1,4 @@
+library(plyr)
 if(!dir.exists('./Data_Storage')) dir.create('./Data_Storage')
 setwd('./Data_Storage')
 for(year in 1990:2016) {
@@ -19,7 +20,7 @@ data.gallery.metal <- list()
 data.gallery.metal <- sapply(1990:2016, function(year) {
         # Read file.
         filename <- paste0('annual_all_', year, '.csv')
-        dat <- read.csv(filename)[, c(9,10,28,51)]
+        dat <- read.csv(filename, stringsAsFactors = FALSE)[, c(9,10,28,51)]
         # Create sub-list.
         sublist <- list()
         
@@ -28,13 +29,15 @@ data.gallery.metal <- sapply(1990:2016, function(year) {
                            dat$Parameter.Name), ]
         # Re-form data with unique coordinates.
         reform1 <- aggregate(subdat$Arithmetic.Mean, 
-                            by = list(State = subdat$State.Name),
-                            FUN = mean)
+                             by = list(State = subdat$State.Name),
+                             FUN = function(i) ifelse(mean(i) != 0, mean(i), NA))
         reform2 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = function(i) as.integer(sum(i)/mean(i)))
+                             FUN = function(i) ifelse(mean(i) != 0, sum(i)/mean(i), 0))
         reform <- merge(reform1, reform2, by = 'State')
         names(reform) <- c('State', 'Mean', 'Num.Monitors')
+        reform$hover <- with(reform, paste(State, '<br>', 'Mean:', Mean,
+                                           '<br>', '# of Sites:', Num.Monitors))
         sublist$Ba <- reform
         
         # Be subset.
@@ -42,148 +45,173 @@ data.gallery.metal <- sapply(1990:2016, function(year) {
                            dat$Parameter.Name), ]
         reform1 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = mean)
+                             FUN = function(i) ifelse(mean(i) != 0, mean(i), NA))
         reform2 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = function(i) as.integer(sum(i)/mean(i)))
+                             FUN = function(i) ifelse(mean(i) != 0, sum(i)/mean(i), 0))
         reform <- merge(reform1, reform2, by = 'State')
         names(reform) <- c('State', 'Mean', 'Num.Monitors')
+        reform$hover <- with(reform, paste(State, '<br>', 'Mean:', Mean,
+                                           '<br>', '# of Sites:', Num.Monitors))
         sublist$Be <- reform
         
         # Cd subset.
         subdat <- dat[grep('Cadmium', dat$Parameter.Name), ]
         reform1 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = mean)
+                             FUN = function(i) ifelse(mean(i) != 0, mean(i), NA))
         reform2 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = function(i) as.integer(sum(i)/mean(i)))
+                             FUN = function(i) ifelse(mean(i) != 0, sum(i)/mean(i), 0))
         reform <- merge(reform1, reform2, by = 'State')
         names(reform) <- c('State', 'Mean', 'Num.Monitors')
+        reform$hover <- with(reform, paste(State, '<br>', 'Mean:', Mean,
+                                           '<br>', '# of Sites:', Num.Monitors))
         sublist$Cd <- reform
         
         # Ca subset.
         subdat <- dat[grep('Calcium', dat$Parameter.Name), ]
         reform1 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = mean)
+                             FUN = function(i) ifelse(mean(i) != 0, mean(i), NA))
         reform2 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = function(i) as.integer(sum(i)/mean(i)))
+                             FUN = function(i) ifelse(mean(i) != 0, sum(i)/mean(i), 0))
         reform <- merge(reform1, reform2, by = 'State')
         names(reform) <- c('State', 'Mean', 'Num.Monitors')
+        reform$hover <- with(reform, paste(State, '<br>', 'Mean:', Mean,
+                                           '<br>', '# of Sites:', Num.Monitors))
         sublist$Ca <- reform
         
         # Cr subset.
         subdat <- dat[grep('Chromium', dat$Parameter.Name), ]
         reform1 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = mean)
+                             FUN = function(i) ifelse(mean(i) != 0, mean(i), NA))
         reform2 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = function(i) as.integer(sum(i)/mean(i)))
+                             FUN = function(i) ifelse(mean(i) != 0, sum(i)/mean(i), 0))
         reform <- merge(reform1, reform2, by = 'State')
         names(reform) <- c('State', 'Mean', 'Num.Monitors')
+        reform$hover <- with(reform, paste(State, '<br>', 'Mean:', Mean,
+                                           '<br>', '# of Sites:', Num.Monitors))
         sublist$Cr <- reform
         
         # Cu subset.
         subdat <- dat[grep('Copper', dat$Parameter.Name), ]
         reform1 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = mean)
+                             FUN = function(i) ifelse(mean(i) != 0, mean(i), NA))
         reform2 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = function(i) as.integer(sum(i)/mean(i)))
+                             FUN = function(i) ifelse(mean(i) != 0, sum(i)/mean(i), 0))
         reform <- merge(reform1, reform2, by = 'State')
         names(reform) <- c('State', 'Mean', 'Num.Monitors')
+        reform$hover <- with(reform, paste(State, '<br>', 'Mean:', Mean,
+                                           '<br>', '# of Sites:', Num.Monitors))
         sublist$Cu <- reform
         
         # Fe subset.
         subdat <- dat[grep('Iron', dat$Parameter.Name), ]
         reform1 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = mean)
+                             FUN = function(i) ifelse(mean(i) != 0, mean(i), NA))
         reform2 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = function(i) as.integer(sum(i)/mean(i)))
+                             FUN = function(i) ifelse(mean(i) != 0, sum(i)/mean(i), 0))
         reform <- merge(reform1, reform2, by = 'State')
         names(reform) <- c('State', 'Mean', 'Num.Monitors')
+        reform$hover <- with(reform, paste(State, '<br>', 'Mean:', Mean,
+                                           '<br>', '# of Sites:', Num.Monitors))
         sublist$Fe <- reform
         
         # Pb subset.
         subdat <- dat[grep('Lead', dat$Parameter.Name), ]
         reform1 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = mean)
+                             FUN = function(i) ifelse(mean(i) != 0, mean(i), NA))
         reform2 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = function(i) as.integer(sum(i)/mean(i)))
+                             FUN = function(i) ifelse(mean(i) != 0, sum(i)/mean(i), 0))
         reform <- merge(reform1, reform2, by = 'State')
         names(reform) <- c('State', 'Mean', 'Num.Monitors')
+        reform$hover <- with(reform, paste(State, '<br>', 'Mean:', Mean,
+                                           '<br>', '# of Sites:', Num.Monitors))
         sublist$Pb <- reform
         
         # Mn subset.
         subdat <- dat[grep('Manganese', dat$Parameter.Name), ]
         reform1 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = mean)
+                             FUN = function(i) ifelse(mean(i) != 0, mean(i), NA))
         reform2 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = function(i) as.integer(sum(i)/mean(i)))
+                             FUN = function(i) ifelse(mean(i) != 0, sum(i)/mean(i), 0))
         reform <- merge(reform1, reform2, by = 'State')
         names(reform) <- c('State', 'Mean', 'Num.Monitors')
+        reform$hover <- with(reform, paste(State, '<br>', 'Mean:', Mean,
+                                           '<br>', '# of Sites:', Num.Monitors))
         sublist$Mn <- reform
         
         # Mo subset.
         subdat <- dat[grep('Molybdenum', dat$Parameter.Name), ]
         reform1 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = mean)
+                             FUN = function(i) ifelse(mean(i) != 0, mean(i), NA))
         reform2 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = function(i) as.integer(sum(i)/mean(i)))
+                             FUN = function(i) ifelse(mean(i) != 0, sum(i)/mean(i), 0))
         reform <- merge(reform1, reform2, by = 'State')
         names(reform) <- c('State', 'Mean', 'Num.Monitors')
+        reform$hover <- with(reform, paste(State, '<br>', 'Mean:', Mean,
+                                           '<br>', '# of Sites:', Num.Monitors))
         sublist$Mo <- reform
         
         # Ni subset.
         subdat <- dat[grep('Nickel', dat$Parameter.Name), ]
         reform1 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = mean)
+                             FUN = function(i) ifelse(mean(i) != 0, mean(i), NA))
         reform2 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = function(i) as.integer(sum(i)/mean(i)))
+                             FUN = function(i) ifelse(mean(i) != 0, sum(i)/mean(i), 0))
         reform <- merge(reform1, reform2, by = 'State')
         names(reform) <- c('State', 'Mean', 'Num.Monitors')
+        reform$hover <- with(reform, paste(State, '<br>', 'Mean:', Mean,
+                                           '<br>', '# of Sites:', Num.Monitors))
         sublist$Ni <- reform
         
         # V subset.
         subdat <- dat[grep('Vanadium', dat$Parameter.Name), ]
         reform1 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = mean)
+                             FUN = function(i) ifelse(mean(i) != 0, mean(i), NA))
         reform2 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = function(i) as.integer(sum(i)/mean(i)))
+                             FUN = function(i) ifelse(mean(i) != 0, sum(i)/mean(i), 0))
         reform <- merge(reform1, reform2, by = 'State')
         names(reform) <- c('State', 'Mean', 'Num.Monitors')
+        reform$hover <- with(reform, paste(State, '<br>', 'Mean:', Mean,
+                                           '<br>', '# of Sites:', Num.Monitors))
         sublist$V <- reform
         
         # Zn subset.
         subdat <- dat[grep('Zinc', dat$Parameter.Name), ]
         reform1 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = mean)
+                             FUN = function(i) ifelse(mean(i) != 0, mean(i), NA))
         reform2 <- aggregate(subdat$Arithmetic.Mean, 
                              by = list(State = subdat$State.Name),
-                             FUN = function(i) as.integer(sum(i)/mean(i)))
+                             FUN = function(i) ifelse(mean(i) != 0, sum(i)/mean(i), 0))
         reform <- merge(reform1, reform2, by = 'State')
         names(reform) <- c('State', 'Mean', 'Num.Monitors')
+        reform$hover <- with(reform, paste(State, '<br>', 'Mean:', Mean,
+                                           '<br>', '# of Sites:', Num.Monitors))
         sublist$Zn <- reform
         
         sublist
 })
+
 setwd('..')
 unlink('Data_Storage', recursive = TRUE)
 # Save list in .RData file for further read.
